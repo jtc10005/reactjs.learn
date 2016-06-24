e3768233b8a70c028901cd6b063c269534a6062d
Jay Culpepper <jtculpepper@buckman.com>
2016-06-23 21:14:01 -0500

    install gulp and some basic react stuff

---
+++ reactjs.test/gulpfile.js
@@ -0,0 +1,33 @@
+﻿var gulp = require('gulp');
+//var jshint = require('gulp-jshint');
+//var jscs = require('gulp-jscs');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var config = {
+  client: 'localhost:3136/Index.html',
+  port:'3136'
+}
+
+gulp.task('default', function () {
+  console.log('Gulp Starting');
+  startBrowserSync();
+
+
+});
+// watch files for changes and reload
+
+function startBrowserSync() {
+  if (browserSync.active) {
+    return;
+  }
+
+  console.log('Starting Browser-Sync from: ' + config.client);
+  var options = {
+    proxy: config.client,
+    port: config.port,
+    files: ['**/*.*'],
+    injectChanges: true,
+    reloadDelay: 1000
+  } 
+  browserSync(options);
+}
